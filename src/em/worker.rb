837caa7c1617021e4d85facc5fcdaecd3b16c2aa
John Murray <me@johnmurray.io>
2012-09-25 21:05:28 -0400

    Added Comments to Worker
    While the first-pass was mostly a thought exercise, it was still
    missing some good comments in case I come back to this later and
    have no idea what I was _thinking_ in my _thought exercise_. LOL
    
    Anyways, the comment structure _sort-of_ follows the [TomDoc][1]
    style, but I'm sure it's not _to spec_.
    
      [1]: http://tomdoc.org

--- src/em/worker.rb
+++ src/em/worker.rb
@@ -1,40 +1,94 @@
-# TODO add better commets (TomDoc style! :-])
 class EM::Worker
 
-  # used to set scheduler in class-definition of child-class
-  def self.scheduler(scheduler, &block)
+  # Public: Set the scheduler that the worker will use to schedule itself
+  #
+  # scheduler - symbol representing any of the available schedulers
+  #             (defaults to :continual)
+  # block     - optional block that will be passed to the scheduler. See
+  #             the scheduler specific documentation to see if/what-kind-of
+  #             block can/should be used.
+  #
+  # Examples
+  #
+  #   class MyWorker < EM::Worker
+  #     scheduler :continual
+  #   end
+  #
+  #   class MyWorker < EM::Worker
+  #     scheduler :periodic do
+  #       time = Time.now
+  #       if time.peak_traffic?
+  #         1.sec
+  #       else
+  #         30.sec
+  #       end
+  #     end
+  #   end
+  #
+  # Returns and instance of the scheduler being used.
+  # Raises RuntimeError if provided scheduler is not a valid scheduler.
+  def self.scheduler(scheduler = :continual, &block)
     @scheduler = case scheduler
                  when :continual; ContinualScheduler.new(&block)
                  when :periodic;  PeriodicScheduler.new(&block)
                  when :scalable;  ScalableScheduler.new(&block)
+                 else raise "Invalid scheduler provided #{scheduler.inspect}"
                  end
   end
 
 
-  # Run the worker in stand-alone mode
+  # Public: Run the worker process outside of an explicit EM loop. It will
+  # start a reactor and tell the scheduler to start scheduling (thus the
+  # worker will start working!)
+  #
+  # Examples
+  #
+  #   class MyWorker < EM::Worker
+  #     # ... code ...
+  #     standalone! if $0 == __FILE__
+  #   end
+  #   
+  #   # or
+  #   
+  #   class MyWorker < EM::Worker
+  #     # ... code ...
+  #   end
+  #   MyWorker.standalone!
+  #   
+  # Returns value of worker.schedule!
   def self.standalone!
     worker = self.new
-    if worker.respond_to? :do_work
-      worker.schedule!
-    end
+    worker.schedule!
   end
 
 
-  # Should be called when work in child-class is complete (up to
-  # the client/implementer to call this method).
+  # Public: Is required to be called by the client worker-class when they
+  # worker is done working (for this round anyways) so that the worker can
+  # be re-scheduled.
+  #
+  # Returns value of worker.schedule!
   def done
     schedule!
   end
   
 
-  #Schedule the worker against the scheduler
+  private
+
+  # Private: Schedule the worker against the worker's scheduler.
+  #
+  # Returns result of @scheduler.schedule.
+  # Raises RuntimeError if worker does not implement 'do_work' method.
   def schedule!
-    @scheduler.schedule(self)
+    if worker.respond_to? :do_work
+      @scheduler.schedule(self)
+    else
+      raise 'Worker does not implement "do_work" method'
+    end
   end
 
 
 
-  # some defaults
+  # set the default values for a Worker
   scheduler :continual
 
 end
